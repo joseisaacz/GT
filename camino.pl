a81bd9a4fc85453ce6ae6a9f33d5d1aef2cab2d2
Jonathan <jjestradav@gmail.com>
2019-11-11 23:35:26 -0600

    SOme changes

--- camino.pl
+++ camino.pl
@@ -20,6 +20,15 @@ conexo(G,false):- quitaPeso(G,L), flatten(L,L1), eliminar(L1,[],L3), domino(L3,[
 conexo(G,true):- quitaPeso(G,L), flatten(L,L1), eliminar(L1,[],L3), domino(L3,[X,Y]),  camino(L,X,Y,_),true,!.
 
 
+%si X tiene una arista dirigida a Y.
+adyacente(X,Y,G) :- member([X,Y],G).
+
+ciclo(G,P):-quitaPeso(G,L), flatten(L,L1),eliminar(L1,[],L3),member(X,L3),ciclo_aux(L,X,P).
+
+ciclo_aux(G,A,P) :- 
+   adyacente(B,A,G), camino(G,A,B,P1), length(P1,L), L > 1, append(P1,[A],P),!.
+
+%valida si todos los nodos son de grado mayor a 1
 valida_hamilton([[_A,B]|_T],E):-B==E,true,!.
 valida_hamilton([[_A,B]|[]],E):-B==E,true,!.
 valida_hamilton([[_A,B]|T],E):-B\==E,valida_hamilton(T,E),!.
@@ -32,22 +41,20 @@ quitaPeso_aux2([],A,A):-!.
 quitaPeso_aux2([H|T],A,L):-quitaPeso_aux1(H,L1),append(A,L1,L2),quitaPeso_aux2(T,L2,L).
 quitaPeso_aux1([H1,T1,_],L):-append([],[[H1,T1]],L).
 
-eliminar([],A,A):-!.
-eliminar([H|T],A,L):-member(H,T),eliminar(T,A,L).
-eliminar([H|T],A,L):- \+ member(H,T),append(A,[H],L1),eliminar(T,L1,L2),sort(L2,L).
+eliminar([],A,A1):-sort(A,A1),!.
+eliminar([H|T],A,L):-member(H,T),eliminar(T,A,L),!.
+eliminar([H|T],A,L):- \+ member(H,T),append(A,[H],L1),eliminar(T,L1,L),!.
 
 my_member(A,G):-member(A,G).
 
 matriz(G,L):-quitaPeso(G,L1),flatten(L1,L2),eliminar(L2,[],L3),product(L3,L3,L4),length(L3,LE),
-matriz_aux(L4,L1,LE,LE,[],[],LE,L).
 
+matriz_aux(L4,L1,LE,LE,[],[],LE,L).
 matriz_aux([],_G,_,_,A,_A1,_O,A):-!.
 matriz_aux([H|T],G,1,C,A,A1,O,L):-member(H,G)-> C1 is C-1,append(A1,[1],A12),append(A,[A12],A2), matriz_aux(T,G,O,C1,A2,[],O,L),!;
 C1 is C-1,append(A1,[0],A12),append(A,[A12],A2), matriz_aux(T,G,O,C1,A2,[],O,L),!.
 matriz_aux([H|T],G,F,C,A,A1,O,L):- member(H,G)->  F1 is F-1,append(A1,[1],A2),matriz_aux(T,G,F1,C,A,A2,O,L),!;
  F1 is F-1,append(A1,[0],A3),matriz_aux(T,G,F1,C,A,A3,O,L),!.
-
-ejemplo(L,L1):-nth(1,L,_)->L1 is 1 ; L1 is 0. 
 
 product(A,B,C) :-
     findall([X,Y],(member(X,A),member(Y,B)),C).
