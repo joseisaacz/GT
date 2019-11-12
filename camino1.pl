e8063200140f4cfa52b9fcd8a9b0f618a11df3bf
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-11-11 21:46:53 -0600

    cc

--- camino1.pl
+++ camino1.pl
@@ -1,10 +1,5 @@
-comb(L, K, L):- length(L,K), !.
-comb(_L,0,[]):-!.
-comb([H|T],K,[H|TC]):- K1 is K-1, comb(T, K1, TC).
-comb([_|T],K,Co):-comb(T,K,Co).
-
 camino(G,A,B,[A,B]):-member([A,B],G).
-camino(G,A,B,[A|C]):-member([A,X],G), camino(G,X,B,C), \+ member(A,C), !.
+camino(G,A,B,[A|C]):-member([A,X],G), camino(G,X,B,C), \+ member(A,C).
 
 %camino(G,A,B,[ ]):- \+member([A,B],G),!.
 %camino(G,A,B,[A,B]):-member([A,B],G).
@@ -30,13 +25,18 @@ C1 is C-1,append(A1,[0],A12),append(A,[A12],A2), matriz_aux(T,G,O,C1,A2,[],O,L),
 matriz_aux([H|T],G,F,C,A,A1,O,L):- member(H,G)->  F1 is F-1,append(A1,[1],A2),matriz_aux(T,G,F1,C,A,A2,O,L);
  F1 is F-1,append(A1,[0],A3),matriz_aux(T,G,F1,C,A,A3,O,L).
 
-
-
 product(A,B,C) :-
     findall([X,Y],(member(X,A),member(Y,B)),C).
 
 domino(L,[X,Y]):-member(X,L), member(Y,L), X\=Y.
 
+conexo(G):- quitaPeso(G,L), listanodos(G,L3), domino(L3,[X,Y]), \+camino(L,X,Y,_), !. 
 
-conexo(G):- quitaPeso(G,L), flatten(L,L1), eliminar(L1,[],L3), domino(L3,[X,Y]), camino(L,X,Y,_). 
+listanodos(G,L):- quitaPeso(G, L0), flatten(L0, L1), eliminar(L1,[],L).
+
+busca(G,A,B,[A,B]):-conexo(G), quitaPeso(G,L), member([A,B],L).
+busca(G,A,B,[A|C]):-conexo(G), quitaPeso(G,L), member([A,X],L), camino(G,X,B,C), \+ member(A,C),!.
+
+%hamilton(G, Camino, Costo):-conexo(G), listanodos(G, [H|T]), quitaPeso(G,[[A,B]|R]), \+member(H,Camino).    
+%hamilton(G, Camino, Costo):- conexo(G), listanodos(G, L1), quitaPeso(G,L2).
 
